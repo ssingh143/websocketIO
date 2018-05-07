d48212b6e2193a3dd632409f1ea53169a36a997a
shiv singh <shiv007singh@gmail.com>
2018-05-07 23:50:47 +0530

    Add connect and diconnect event hanlers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app)
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+    console.log("new user connected");
+    socket.on('disconnect', (socket) => {
+        console.log("User was diconnected");
+    })
+})
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
